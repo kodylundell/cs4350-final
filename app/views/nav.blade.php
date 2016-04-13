7c9b028e2a8a1bbcf7f8038f9be5942369c626dc
Xyleroth <beadleshayden@gmail.com>
2016-04-13 17:49:08 -0600

    Nav Blade Change

--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -1,13 +1,13 @@
-@extends("layout.master")
-
-@section('title', "Title Page")
-
-@section('nav')
-
-
-
-
-
-
-
-@endsection
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Main Project</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</nav>
